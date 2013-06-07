47483ec68471b2aa5d864c98738b1ad93171c323
Carl-Johan Kjellander <carl-johan@campanja.com>
2013-06-07 11:29:53 +0200

    add my old C sig-file
    Try and find out how it works, and from where I have stolen the idea.

---
+++ sig.c
@@ -0,0 +1,4 @@
+main(w,_,a)char*a;{return 1<w?0x7d==*a||main(/*f95cakj@dd.chalmers.se*/
+w,main(-1,*a,"Qw[^@x%|a!=#*r]\nljrJnCa he-oKd"),/*or: spiff@iname.com*/
+1+a):0<w?main(3,w,"%|^w#@*!|xar[=ww|x]=^Q}"):_==*a/*ICQ#(UIN) 5775899*/
+?putchar(0xf[a]):main(w,_,a+1);}/*http://www.dd.chalmers.se/~f95cakj */
